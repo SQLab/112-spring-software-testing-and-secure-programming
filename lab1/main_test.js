8785d5dbef41ff5f218f266a8505a8532f9fea7d
LYX <joy9517538246@gmail.com>
2024-03-07 19:57:52 +0800

    TODO

--- lab1/main_test.js
+++ lab1/main_test.js
@@ -2,22 +2,36 @@ const test = require('node:test');
 const assert = require('assert');
 const { MyClass, Student } = require('./main');
 
+const myClass = new MyClass();
+const studentId = 1;
+
 test("Test MyClass's addStudent", () => {
-    // TODO
-    throw new Error("Test not implemented");
+    
+    
+    const names = ['John', 'Jane', 'Doe', 'Smith'];
+    names.forEach(name => {
+    const student = new Student();
+    student.setName(name);
+    const newStudentId = myClass.addStudent(student);
+    const newStudentName = myClass.getStudentById(newStudentId).getName();
+    console.log('[+] Added student with id: %d, name: %s', newStudentId, newStudentName);
+    });
 });
 
 test("Test MyClass's getStudentById", () => {
-    // TODO
-    throw new Error("Test not implemented");
+    
+    const StudentName = myClass.getStudentById(studentId).getName();
+    console.log('[+] Get student with id: %d, name: %s', 1, StudentName);
+    
 });
 
 test("Test Student's setName", () => {
-    // TODO
-    throw new Error("Test not implemented");
+    const newStudentName = "Jimmy";
+    myClass.getStudentById(1).setName(newStudentName);
+    console.log('[+] Set student name: %s', newStudentName);
 });
 
 test("Test Student's getName", () => {
-    // TODO
-    throw new Error("Test not implemented");
+    const studentName = myClass.getStudentById(1).getName();
+    console.log('[+] Get student name: %s', studentName);
 });
