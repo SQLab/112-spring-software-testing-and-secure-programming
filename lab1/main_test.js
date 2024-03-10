f9290b1a8676649d15225023b253eaf9979727ae
root <root@ubunt.myguest.virtualbox.org>
2024-03-10 12:52:15 +0800

    0310

--- lab1/main_test.js
+++ lab1/main_test.js
@@ -2,22 +2,38 @@ const test = require('node:test');
 const assert = require('assert');
 const { MyClass, Student } = require('./main');
 
-test("Test MyClass's addStudent", () => {
-    // TODO
-    throw new Error("Test not implemented");
+test("test MyClass addStudent", async (t) => {
+    const myClass = new MyClass();
+    const student = new Student();
+    student.setName("A");
+    const index = myClass.addStudent(student);
+
+    assert.strictEqual(myClass.students[index], student);
+
+
 });
 
-test("Test MyClass's getStudentById", () => {
-    // TODO
-    throw new Error("Test not implemented");
+test("test MyClass getStudentById", async (t) => {
+    const myClass = new MyClass();
+    const student = new Student();
+    student.setName("B");
+    const index = myClass.addStudent(student);
+
+    const foundStudent = myClass.getStudentById(index);
+    assert.strictEqual(foundStudent, student);
 });
 
-test("Test Student's setName", () => {
-    // TODO
-    throw new Error("Test not implemented");
+test("test Student setName", async (t) => {
+    const student = new Student();
+    student.setName("A");
+
+    assert.strictEqual(student.getName(), "A");
 });
 
-test("Test Student's getName", () => {
-    // TODO
-    throw new Error("Test not implemented");
+test("test Student getName", async (t) => {
+    const student = new Student();
+    student.setName("B");
+
+    assert.strictEqual(student.getName(), "B");
 });
+
