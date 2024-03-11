c01bd69d0120152d419203ca1e5a766afe419d16
pleaseppu <94917437+pleaseppu@users.noreply.github.com>
2024-03-11 22:05:22 +0800

    main_test.js

--- lab1/main_test.js
+++ lab1/main_test.js
@@ -5,16 +5,16 @@ const { MyClass, Student } = require('./main');
 test("Test MyClass's addStudent", () => {
   const myClass = new MyClass();
   const student1 = new Student();
-  student1.setName('Chris');
+  student1.setName('John');
   const id1 = myClass.addStudent(student1);
   assert.strictEqual(id1, 0);
 
   const student2 = new Student();
-  student2.setName('Wayne');
+  student2.setName('Jane');
   const id2 = myClass.addStudent(student2);
   assert.strictEqual(id2, 1);
 
-  const invalidStudent = 'Test';
+  const invalidStudent = 'TESTING';
   const invalidId = myClass.addStudent(invalidStudent);
   assert.strictEqual(invalidId, -1);
 });
@@ -22,11 +22,11 @@ test("Test MyClass's addStudent", () => {
 test("Test MyClass's getStudentById", () => {
   const myClass = new MyClass();
   const student1 = new Student();
-  student1.setName('Chris');
+  student1.setName('John');
   myClass.addStudent(student1);
 
   const retrievedStudent = myClass.getStudentById(0);
-  assert.strictEqual(retrievedStudent.getName(), 'Wayne');
+  assert.strictEqual(retrievedStudent.getName(), 'John');
 
   const invalidStudent = myClass.getStudentById(-1);
   assert.strictEqual(invalidStudent, null);
@@ -38,17 +38,19 @@ test("Test MyClass's getStudentById", () => {
 test("Test Student's setName", () => {
   const student = new Student();
 
-  student.setName('Danny');
-  assert.strictEqual(student.getName(), 'Danny');
+  student.setName('Alice');
+  assert.strictEqual(student.getName(), 'Alice');
 
-  student.setName(1234546); 
-  assert.strictEqual(student.getName(), 'Danny');
+  student.setName(123); 
+  assert.strictEqual(student.getName(), 'Alice');
+
 });
 
 test("Test Student's getName", () => {
   const student = new Student();
 
   assert.strictEqual(student.getName(), '');
-  student.setName('JK');
-  assert.strictEqual(student.getName(), 'JK');
+
+  student.setName('Bob');
+  assert.strictEqual(student.getName(), 'Bob');
 });
