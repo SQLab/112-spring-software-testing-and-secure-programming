aefb68200f480161cc6df6a25d47afb42102592b
KentYang123 <94535684+KentYang123@users.noreply.github.com>
2024-03-11 19:29:18 +0800

    Update main_test.js

--- lab1/main_test.js
+++ lab1/main_test.js
@@ -3,21 +3,25 @@ const assert = require('assert');
 const { MyClass, Student } = require('./main');
 
 test("Test MyClass's addStudent", () => {
-    // TODO
-    throw new Error("Test not implemented");
-});
+  const myClass = new MyClass();
+  const student1 = new Student();
+  student1.setName('John');
+  const id1 = myClass.addStudent(student1);
+  assert.strictEqual(id1, 0);
 
-test("Test MyClass's getStudentById", () => {
-    // TODO
-    throw new Error("Test not implemented");
-});
+  const student2 = new Student();
+  student2.setName('Jane');
+  const id2 = myClass.addStudent(student2);
+  assert.strictEqual(id2, 1);
+  //新增兩個有效的student，測試是否正確
 
-test("Test Student's setName", () => {
-    // TODO
-    throw new Error("Test not implemented");
-});
+  const invalidStudent = 'TESTING';
+  const invalidId = myClass.addStudent(invalidStudent);
+  assert.strictEqual(invalidId, -1);
+  //新增無效的student，應返回-1
 
-test("Test Student's getName", () => {
-    // TODO
-    throw new Error("Test not implemented");
+  const invalidStudentObject = { name: 'InvalidStudent' };
+  const invalidIdObject = myClass.addStudent(invalidStudentObject);
+  assert.strictEqual(invalidIdObject, -1);
+  //新增非Student對象，應返回-1
 });
