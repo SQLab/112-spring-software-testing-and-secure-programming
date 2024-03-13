6cd513663d97bf352e3c2bdfaa6e01181fc1e989
funiris <funiris@gmail.com>
2024-03-13 06:28:18 -0700

    0313

--- lab1/main_test.js
+++ lab1/main_test.js
@@ -1,23 +1,57 @@
 const test = require('node:test');
 const assert = require('assert');
 const { MyClass, Student } = require('./main');
-//test
+
 test("Test MyClass's addStudent", () => {
-    // TODO
-    throw new Error("Test not implemented");
+    const myClass = new MyClass();
+    const student = new Student();
+    
+    // Adding a valid student
+    const validStudentId = myClass.addStudent(student);
+    assert.strictEqual(validStudentId, 0);
+
+    // Adding an invalid student
+    const invalidStudentId = myClass.addStudent({});
+    assert.strictEqual(invalidStudentId, -1);
 });
 
 test("Test MyClass's getStudentById", () => {
-    // TODO
-    throw new Error("Test not implemented");
+    const myClass = new MyClass();
+    const student1 = new Student();
+    const student2 = new Student();
+    student1.setName('John');
+    student2.setName('Jane');
+    myClass.addStudent(student1);
+    myClass.addStudent(student2);
+
+    // Getting an existing student
+    const existingStudent = myClass.getStudentById(1);
+    assert.strictEqual(existingStudent.getName(), 'Jane');
+
+    // Getting a non-existing student
+    const nonExistingStudent = myClass.getStudentById(2);
+    assert.strictEqual(nonExistingStudent, null);
 });
 
 test("Test Student's setName", () => {
-    // TODO
-    throw new Error("Test not implemented");
+    const student = new Student();
+    
+    // Setting a valid name
+    student.setName('John');
+    assert.strictEqual(student.getName(), 'John');
+
+    // Setting an invalid name
+    student.setName(123); // Setting a non-string name
+    assert.strictEqual(student.getName(), ''); // Name should remain unchanged
 });
 
 test("Test Student's getName", () => {
-    // TODO
-    throw new Error("Test not implemented");
+    const student = new Student();
+
+    // Getting name when it's not set
+    assert.strictEqual(student.getName(), '');
+
+    // Getting name after setting it
+    student.setName('Jane');
+    assert.strictEqual(student.getName(), 'Jane');
 });
