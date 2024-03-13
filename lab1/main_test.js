2409e230b06b5f2b3e82f6f8959d0cfa493914f8
Oliver <162861141+Oliver84586454@users.noreply.github.com>
2024-03-13 10:02:38 +0800

    Update main_test.js

--- lab1/main_test.js
+++ lab1/main_test.js
@@ -1,43 +1,61 @@
-const test = require('node:test');
-const assert = require('assert');
-const { MyClass, Student } = require('./main');
+        const test = require('node:test');
+		const assert = require('assert');
+		const { MyClass, Student } = require('./main');
+		
 
-test("Test MyClass's addStudent", () => {
-    // TODO
-    throw new Error("Test not implemented");
-    const myClass = new MyClass();
-    const student = new Student();
-    const newStudentId = myClass.addStudent(student);
-    assert.strictEqual(newStudentId, 0);
-    assert.strictEqual(myClass.addStudent('not a student'), -1);
-});
+		test("Test MyClass's addStudent", () => {
+		// TODO
+		throw new Error("Test not implemented");
+		const myClass = new MyClass();
+		const student = new Student();
+		student.setName('John Doe');
+		const index = myClass.addStudent(student);
+		assert.strictEqual(index, 0);
+		
 
-test("Test MyClass's getStudentById", () => {
-    // TODO
-    throw new Error("Test not implemented");
-    const myClass = new MyClass();
-    const student = new Student();
-    myClass.addStudent(student);
-    assert.strictEqual(myClass.getStudentById(0), student);
-    assert.strictEqual(myClass.getStudentById(999), null);
-});
+		const nonStudent = {};
+		const wrongIndex = myClass.addStudent(nonStudent);
+		assert.strictEqual(wrongIndex, -1);
+		});
+		
 
-test("Test Student's setName", () => {
-    // TODO
-    throw new Error("Test not implemented");
-    const student = new Student();
-    student.setName('John');
-    assert.strictEqual(student.getName(), 'John');
-    student.setName(123);
-    assert.strictEqual(student.getName(), 'John'); 
-});
+		test("Test MyClass's getStudentById", () => {
+		// TODO
+		throw new Error("Test not implemented");
+		const myClass = new MyClass();
+		const student = new Student();
+		student.setName('Jane Doe');
+		const index = myClass.addStudent(student);
+		const retrievedStudent = myClass.getStudentById(index);
+		assert.strictEqual(retrievedStudent, student);
+		
 
-test("Test Student's getName", () => {
-    // TODO
-    throw new Error("Test not implemented");
-    const student = new Student();
-    assert.strictEqual(student.getName(), '');
-    student.setName('John');
-    assert.strictEqual(student.getName(), 'John');
-});
-    
+		const nullStudent = myClass.getStudentById(-1);
+		assert.strictEqual(nullStudent, null);
+		});
+		
+
+		test("Test Student's setName", () => {
+		// TODO
+		throw new Error("Test not implemented");
+		const student = new Student();
+		student.setName('John Doe');
+		assert.strictEqual(student.getName(), 'John Doe');
+		
+
+		student.setName(123); // 嘗試設置非字串
+		assert.strictEqual(student.getName(), 'John Doe');
+		});
+		
+
+		test("Test Student's getName", () => {
+		// TODO
+		throw new Error("Test not implemented");
+		});
+		const student = new Student();
+		assert.strictEqual(student.getName(), '');
+		
+
+		student.setName('Jane Doe');
+		assert.strictEqual(student.getName(), 'Jane Doe');
+		});
