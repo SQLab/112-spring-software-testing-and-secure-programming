491abe7cca0dafc45e1a8ac5e4d3f1bffae2cae1
root <root@Ubuntu20.4.myguest.virtualbox.org>
2024-03-16 23:21:05 +0800

    0316

--- lab2/main_test.js
+++ lab2/main_test.js
@@ -2,5 +2,5 @@ const test = require('node:test');
 const assert = require('assert');
 const { Application, MailSystem } = require('./main');
 
-// TODO: write your tests here
+// TODO: write your tests here test
 // Remember to use Stub, Mock, and Spy when necessary
