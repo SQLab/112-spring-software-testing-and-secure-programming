f5b2549d8a004cc371a836371f5d1caf8efae6bd
CHIH MING CHEN <129422182+Yyrff@users.noreply.github.com>
2024-03-19 13:25:52 +0800

    main_test.js

--- lab2/main_test.js
+++ lab2/main_test.js
@@ -2,9 +2,10 @@ const test = require('node:test');
 const assert = require('assert');
 const { Application, MailSystem } = require('./main');
 
-test.mock.method(fs, 'readFile', (file, options, callback) => {
-
+test.mock.method(fs, 'readFile', (file, options, callback) => 
+  {
   callback(null, 'alpha\nbeta\ngama');
-});
+  }
+                );
 
 const { Application, MailSystem } = require('./main');
