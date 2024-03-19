31a6e6e6083b411c1a25051c4f864fa1bff21e99
CHIH MING CHEN <129422182+Yyrff@users.noreply.github.com>
2024-03-19 13:10:48 +0800

    main_test.js

--- lab2/main_test.js
+++ lab2/main_test.js
@@ -1,6 +1,13 @@
 const test = require('node:test');
 const assert = require('assert');
+const fs = require('fs');
+// 模擬 fs.readFile 函式，讓它總是回傳預設內容
+test.mock.method(fs, 'readFile', (file, options, callback) => {
+  // 檔案讀取成功 (null 代表沒有錯誤)
+  callback(null, 'alpha\nbeta\ngama');
+});
+
+// 引入待測試的程式 (./main.js)
 const { Application, MailSystem } = require('./main');
 
-// TODO: write your tests here
-// Remember to use Stub, Mock, and Spy when necessary
+
