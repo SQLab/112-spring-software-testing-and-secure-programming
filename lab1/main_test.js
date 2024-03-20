4f7dd55d3da583c85e8d639ae02f81d1ed504e03
WAT4TA <153968143+WAT4TA@users.noreply.github.com>
2024-03-20 11:15:02 +0800

    Update main_test.js

--- lab1/main_test.js
+++ lab1/main_test.js
@@ -3,21 +3,30 @@ const assert = require('assert');
 const { MyClass, Student } = require('./main');
 
 test("Test MyClass's addStudent", () => {
-    // TODO
-    throw new Error("Test not implemented");
+    const myClass = new MyClass();
+    const student = new Student("John");
+    myClass.addStudent(student);
+    assert.strictEqual(myClass.students.length, 1);
+    assert.strictEqual(myClass.students[0], student);
 });
 
 test("Test MyClass's getStudentById", () => {
-    // TODO
-    throw new Error("Test not implemented");
+    const myClass = new MyClass();
+    const student1 = new Student("John");
+    const student2 = new Student("Alice");
+    myClass.addStudent(student1);
+    myClass.addStudent(student2);
+    const foundStudent = myClass.getStudentById(student2.id);
+    assert.strictEqual(foundStudent, student2);
 });
 
 test("Test Student's setName", () => {
-    // TODO
-    throw new Error("Test not implemented");
+    const student = new Student("John");
+    student.setName("Alice");
+    assert.strictEqual(student.getName(), "Alice");
 });
 
 test("Test Student's getName", () => {
-    // TODO
-    throw new Error("Test not implemented");
+    const student = new Student("John");
+    assert.strictEqual(student.getName(), "John");
 });
