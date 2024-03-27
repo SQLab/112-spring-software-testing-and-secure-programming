6beb3f3c5648dfde75453a18328ca7657f23bd49
CHIH MING CHEN <129422182+Yyrff@users.noreply.github.com>
2024-03-27 18:10:23 +0800

    Update main_test.js

--- lab3/main_test.js
+++ lab3/main_test.js
@@ -2,7 +2,7 @@ const { describe, it } = require('node:test');
 const assert = require('assert');
 const { Calculator } = require('./main');
 
-
+// TODO: write your tests here
 describe('Calculator', () => {
     const calculator = new Calculator();
 
