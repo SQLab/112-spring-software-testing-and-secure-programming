3efc5b8e2672fff05b34473438848ae8529bfff5
DESKTOP-VND7RJG\mnd <tomFP6H@gmail.com>
2024-03-29 22:40:56 +0800

    lab4 v1.2

--- lab4/main_test.js
+++ lab4/main_test.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 
 (async () => {
     // Launch the browser and open a new blank page
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
 
     // Navigate the page to a URL
@@ -10,12 +10,20 @@ const puppeteer = require('puppeteer');
 
     // Hints:
     // Click search button
+    await page.click('button.DocSearch.DocSearch-Button');
     // Type into search box
+    await page.waitForSelector('#docsearch-input');
+    await page.type('#docsearch-input', 'chipi chipi chapa chapa', { delay: 100 });
     // Wait for search result
     // Get the `Docs` result section
+    await page.waitForSelector('#docsearch-item-5');
     // Click on first result in `Docs` section
+    await page.click('#docsearch-item-5');
     // Locate the title
+    let textSelector = await page.waitForSelector('h1');
+    let title = await textSelector.evaluate(element => element.textContent);
     // Print the title
+    console.log(title);
 
     // Close the browser
     await browser.close();
