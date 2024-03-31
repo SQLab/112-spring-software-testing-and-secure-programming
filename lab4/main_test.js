60ccb16bd5a36329a91e6b871b24b219d0d8dafe
tingegg <tingegg8106@gmail.com>
2024-03-31 10:12:30 +0800

    lab4_test3

--- lab4/main_test.js
+++ lab4/main_test.js
@@ -1,37 +1,28 @@
 const puppeteer = require('puppeteer');
-
 (async () => {
     // Launch the browser and open a new blank page
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-
     // Navigate the page to a URL
     await page.goto('https://pptr.dev/');
 
-    // Set screen size
-    await page.setViewport({width: 1080, height: 1024});
-
+    // Hints:
     // Click search button
-    await page.click('.DocSearch.DocSearch-Button');
-
+    await page.click('button.DocSearch.DocSearch-Button');
     // Type into search box
-    await page.type('#docsearch-input', 'chipi chipi chapa chapa');
-
+    await page.waitForSelector('#docsearch-input');
+    await page.type('#docsearch-input', 'chipi chipi chapa chapa', { delay: 100 });
     // Wait for search result
-    const searchResultSelector = '.devsite-result-item-link';
-
     // Get the `Docs` result section
-    await page.waitForSelector('#docsearch-item-5'); 
-
+    await page.waitForSelector('#docsearch-item-5');
     // Click on first result in `Docs` section
-    await page.click('#docsearch-item-5'); 
-
+    await page.click('#docsearch-item-5');
     // Locate the title
-    const textSelector = await page.waitForSelector('h1');  
-    const fullTitle = await textSelector?.evaluate(el => el.textContent);
-
+    let textSelector = await page.waitForSelector('h1');
+    let title = await textSelector.evaluate(element => element.textContent);
     // Print the title
-    console.log(fullTitle);
+    console.log(title);
+
 
     // Close the browser
     await browser.close();
