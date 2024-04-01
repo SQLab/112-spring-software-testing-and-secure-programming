e525fefca12941171bfe84a93524050f87f57724
KentYang123 <94535684+KentYang123@users.noreply.github.com>
2024-04-01 09:16:41 +0800

    Update main_test.js

--- lab4/main_test.js
+++ lab4/main_test.js
@@ -8,14 +8,35 @@ const puppeteer = require('puppeteer');
     // Navigate the page to a URL
     await page.goto('https://pptr.dev/');
 
-    // Hints:
+    // Wait for the search button to appear
+    await page.waitForSelector('.DocSearch.DocSearch-Button');
+
     // Click search button
+    await page.click('.DocSearch.DocSearch-Button');
+
+    // Wait for the search input box to appear
+    await page.waitForSelector('#docsearch-input');
+
     // Type into search box
+    await page.type('#docsearch-input', 'chipi chipi chapa chapa');
+
     // Wait for search result
+    await page.waitForSelector('.devsite-result-item-link');
+
     // Get the `Docs` result section
+    const docSection = await page.$('.devsite-result-item-link');
+
     // Click on first result in `Docs` section
+    await docSection.click();
+
     // Locate the title
+    const titleElement = await page.waitForSelector('h1');
+
+    // Get the text content of the title element
+    const title = await page.evaluate(titleElement => titleElement.textContent, titleElement);
+
     // Print the title
+    console.log(title);
 
     // Close the browser
     await browser.close();
