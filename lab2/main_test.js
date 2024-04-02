6715584db2a5fb3036ca80f2998c5b6961ed08ce
HsuChanLian <163532398+HsuChanLian@users.noreply.github.com>
2024-04-03 06:37:08 +0800

    Update main_test.js

--- lab2/main_test.js
+++ lab2/main_test.js
@@ -4,3 +4,30 @@ const { Application, MailSystem } = require('./main');
 
 // TODO: write your tests here
 // Remember to use Stub, Mock, and Spy when necessary
+
+//  MailSystem()
+var fs = require('fs');
+         fs.readFile("name_list.txt", "utf8", async function (err, data) {
+             await fs.writeFile("name_list.txt", "\nsad\ndas\nasd\ndsa\nads\n", async function (err) {
+                 console.log('寫入完成!');
+
+                 await fs.readFile("name_list.txt", "utf8", async function (err, data) {
+                     console.log('讀取完成！');
+                     const c = new Application().getNames();
+                     const d = new Application().notifySelected();
+                     const e = new Application().getRandomPerson();
+                     const f = new Application().selectNextPerson();
+                     const g = new Application();
+                     await g.getNames();
+                     await g.selectNextPerson();
+                     await g.selectNextPerson();
+                     await g.notifySelected();
+                     console.log(g.notifySelected())
+                 });
+             });
+         });
+// stub
+const a = new MailSystem().write("a");
+const b = new MailSystem().send("b");
+//Mock
+// Spy
