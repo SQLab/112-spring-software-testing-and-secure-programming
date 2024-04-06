dacf9dffa6fce3aac69aed2213550245b36c4e74
KentYang123 <94535684+KentYang123@users.noreply.github.com>
2024-04-06 15:00:18 +0800

    Update main_test.js

--- lab4/main_test.js
+++ lab4/main_test.js
@@ -5,33 +5,47 @@ const puppeteer = require('puppeteer');
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
-    // Navigate the page to a URL
-    await page.goto('https://pptr.dev/');
+    try {
+        // Navigate the page to a URL
+        await page.goto('https://pptr.dev/');
 
-    // Click search button
-    await page.click('.DocSearch.DocSearch-Button');
+	    // Hints:
+    	// Click search button
+    	// Type into search box
+    	// Wait for search result
+    	// Get the `Docs` result section
+    	// Click on first result in `Docs` section
+    	// Locate the title
+    	// Print the title
 
-    // Type into search box
-    await page.type('#docsearch-input', 'chipi chipi chapa chapa');
+        // Click search button
+        await page.click('button.DocSearch.DocSearch-Button');
 
-    // Wait for search result
-    await page.waitForSelector('.devsite-result-item-link');
+        // Type into search box
+        await page.waitForSelector('#docsearch-input');
+        await page.type('#docsearch-input', 'chipi chipi chapa chapa', { delay: 1000 });
 
-    // Get the `Docs` result section
-    const docSection = await page.$('.devsite-result-item-link');
+        // Wait for search result
+        await page.waitForSelector('#docsearch-item-5');
 
-    // Click on first result in `Docs` section
-    await docSection.click();
+        // Click on first result in `Docs` section
+        await page.click('#docsearch-item-5');
 
-    // Locate the title
-    const titleElement = await page.waitForSelector('h1');
+        // Locate the title
+        await page.waitForSelector('h1');
 
-    // Get the text content of the title element
-    const title = await page.evaluate(titleElement => titleElement.textContent, titleElement);
+        // Get the text content of the title element
+        const title = await page.evaluate(() => {
+            const titleElement = document.querySelector('h1');
+            return titleElement.textContent;
+        });
 
-    // Print the title
-    console.log(title);
-
-    // Close the browser
-    await browser.close();
+        // Print the title
+        console.log(title);
+    } catch (error) {
+        console.error('An error occurred:', error);
+    } finally {
+        // Close the browser
+        await browser.close();
+    }
 })();
