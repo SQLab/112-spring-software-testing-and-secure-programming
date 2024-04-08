38336fb00cb6e576470da2bc1f8630c157606309
tnoo436792 <30401693+tnoo436792@users.noreply.github.com>
2024-04-08 08:56:53 +0800

    Update main_test.js

--- lab3/main_test.js
+++ lab3/main_test.js
@@ -2,8 +2,6 @@ const { describe, it } = require('node:test');
 const assert = require('assert');
 const { Calculator } = require('./main');
 
-// TODO: write your tests here
-
 describe('Calculator', () => {
     const calculator = new Calculator();
 
