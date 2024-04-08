34892c9cf65f8dc78c12aac732f900a03cf05671
dandycheng110 <68653768+dandycheng110@users.noreply.github.com>
2024-04-08 09:48:13 -0400

    Update main_test.js

--- lab3/main_test.js
+++ lab3/main_test.js
@@ -2,4 +2,51 @@ const { describe, it } = require('node:test');
 const assert = require('assert');
 const { Calculator } = require('./main');
 
-// TODO: write your tests here
+// Tests for exp function
+describe("Test exp", () => {
+    const calculator = new Calculator();
+    const exp = calculator.exp;
+    const testCases = [
+        { param: "1", expectedError: Error },
+        { param: 1000, expectedError: Error },
+        { param: 2, expected: Math.exp(2) }
+    ];
+    testCases.forEach(({ param, expected, expectedError }) => {
+        if (expectedError) {
+            it("should throw an error", () => {
+                assert.throws(() => {
+                    exp(param);
+                }, expectedError);
+            });
+        } else {
+            it("should return expected value", () => {
+                assert.strictEqual(exp(param), expected);
+            });
+        }
+    });
+});
+
+// Tests for log function
+describe("Test log", () => {
+    const calculator = new Calculator();
+    const log = calculator.log;
+    const testCases = [
+        { param: "1", expectedError: Error },
+        { param: 0, expectedError: Error },
+        { param: -1, expectedError: Error },
+        { param: 2, expected: Math.log(2) }
+    ];
+    testCases.forEach(({ param, expected, expectedError }) => {
+        if (expectedError) {
+            it("should throw an error", () => {
+                assert.throws(() => {
+                    log(param);
+                }, expectedError);
+            });
+        } else {
+            it("should return expected value", () => {
+                assert.strictEqual(log(param), expected);
+            });
+        }
+    });
+});
