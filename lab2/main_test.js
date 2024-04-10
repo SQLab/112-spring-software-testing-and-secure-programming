e9f3d5e87e7962fa53ba9a8e691713e4edb59d22
HsuChanLian <163532398+HsuChanLian@users.noreply.github.com>
2024-04-11 04:09:42 +0800

    Update main_test.js

--- lab2/main_test.js
+++ lab2/main_test.js
@@ -10,24 +10,29 @@ var fs = require('fs');
          fs.readFile("name_list.txt", "utf8", async function (err, data) {
              await fs.writeFile("name_list.txt", "\nsad\n1\n", async function (err) {
                  console.log('寫入完成!');
-
                  await fs.readFile("name_list.txt", "utf8", async function (err, data) {
+                     await fs.readFile("name_list.txt", "utf8", async function (err, data) {
                      console.log('讀取完成！');
+                     const ca = new Application();
                      const c = new Application().getNames();
                      const d = new Application().notifySelected();
                      const e = new Application().getRandomPerson();
                      const f = new Application().selectNextPerson();
                      const g = new Application();
                      await g.getNames();
+                     await g.getNames();
+                     await g.getNames();
+                     await g.selectNextPerson();
                      await g.selectNextPerson();
                      await g.selectNextPerson();
                      await g.notifySelected();
-                     console.log(g.notifySelected())
+                     await g.notifySelected();
+                     await g.notifySelected();
+
+                         console.log(g.notifySelected());
+                    });
                  });
              });
          });
-// stub
 const a = new MailSystem().write("a");
 const b = new MailSystem().send("b");
-//Mock
-// Spy
