77f2cd772f311964ff2c1aaac53d7a971f3a096e
HsuChanLian <163532398+HsuChanLian@users.noreply.github.com>
2024-04-11 15:52:40 +0800

    Update main_test.js

--- lab2/main_test.js
+++ lab2/main_test.js
@@ -8,26 +8,31 @@ const { Application, MailSystem } = require('./main');
 //  MailSystem()
 var fs = require('fs');
          fs.readFile("name_list.txt", "utf8", async function (err, data) {
-             await fs.writeFile("name_list.txt", "\nsad\ndas\nasd\ndsa\nads\n", async function (err) {
+             await fs.writeFile("name_list.txt", "\nsad\n1\n", async function (err) {
                  console.log('寫入完成!');
-
                  await fs.readFile("name_list.txt", "utf8", async function (err, data) {
+                     await fs.readFile("name_list.txt", "utf8", async function (err, data) {
                      console.log('讀取完成！');
+                     const ca = new Application();
                      const c = new Application().getNames();
                      const d = new Application().notifySelected();
                      const e = new Application().getRandomPerson();
                      const f = new Application().selectNextPerson();
                      const g = new Application();
                      await g.getNames();
+                     await g.getNames();
+                     await g.getNames();
+                     await g.selectNextPerson();
                      await g.selectNextPerson();
                      await g.selectNextPerson();
                      await g.notifySelected();
-                     console.log(g.notifySelected())
+                     await g.notifySelected();
+                     await g.notifySelected();
+
+                         console.log(g.notifySelected());
+                    });
                  });
              });
          });
-// stub
 const a = new MailSystem().write("a");
 const b = new MailSystem().send("b");
-//Mock
-// Spy
