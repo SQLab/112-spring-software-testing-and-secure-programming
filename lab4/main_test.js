a253b83a4f0ab6d95fc065b5b27a4f17d3679923
n19971019 <137515554+n19971019@users.noreply.github.com>
2024-04-15 11:14:30 +0800

    Update main_test.js

--- lab4/main_test.js
+++ lab4/main_test.js
@@ -1,22 +1,38 @@
 const puppeteer = require('puppeteer');
 
-(async () => {
-    // Launch the browser and open a new blank page
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
+async function searchAndPrintTitle() {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
 
-    // Navigate the page to a URL
+  try {
     await page.goto('https://pptr.dev/');
 
-    // Hints:
-    // Click search button
-    // Type into search box
-    // Wait for search result
-    // Get the `Docs` result section
-    // Click on first result in `Docs` section
-    // Locate the title
-    // Print the title
+    // 使用 XPath 點擊搜尋按鈕
+    const searchButtonXPath = '//*[@class="DocSearch-Button"]';
+    await page.waitForXPath(searchButtonXPath);
+    const searchButton = await page.$x(searchButtonXPath);
+    await searchButton[0].click();
 
-    // Close the browser
+    // 使用 XPath 輸入搜尋文字
+    const searchInputXPath = '//*[@id="docsearch-input"]';
+    await page.waitForXPath(searchInputXPath);
+    const searchInput = await page.$x(searchInputXPath);
+    await searchInput[0].type('chipi chipi chapa chapa');
+
+    // 使用 XPath 點擊第一個 Docs 結果
+    const firstDocResultXPath = '//*[@id="docsearch-hits"]/div[1]/div/a';
+    await page.waitForXPath(firstDocResultXPath);
+    const firstDocResult = await page.$x(firstDocResultXPath);
+    await firstDocResult[0].click();
+
+    // 使用 page.evaluate 取得標題
+    const title = await page.evaluate(() => document.title);
+    console.log(title);
+  } catch (error) {
+    console.error('Error during search:', error);
+  } finally {
     await browser.close();
-})();
+  }
+}
+
+searchAndPrintTitle();
