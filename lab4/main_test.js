b6022d74971bc117bf16c0d2dcdd6b4cab0b91e3
n19971019 <137515554+n19971019@users.noreply.github.com>
2024-04-15 14:18:45 +0800

    Update main_test.js

--- lab4/main_test.js
+++ lab4/main_test.js
@@ -1,22 +1,38 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-    // Launch the browser and open a new blank page
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
+  // 啟動瀏覽器並開啟新分頁
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
 
-    // Navigate the page to a URL
-    await page.goto('https://pptr.dev/');
+  // 前往指定網址
+  await page.goto('https://pptr.dev/');
 
-    // Hints:
-    // Click search button
-    // Type into search box
-    // Wait for search result
-    // Get the `Docs` result section
-    // Click on first result in `Docs` section
-    // Locate the title
-    // Print the title
+  // 找到並點擊搜尋按鈕
+  const searchButton = await page.$('.DocSearch-Button'); // 使用 CSS 選擇器
+  await searchButton.click();
 
-    // Close the browser
-    await browser.close();
+  // 輸入搜尋文字
+  const searchInput = await page.$('#docsearch-input'); // 使用 CSS 選擇器
+  await searchInput.type('chipi chipi chapa chapa');
+
+  // 等待搜尋結果出現
+  await page.waitForSelector('#docsearch-item-5 > a');
+
+  // 取得文件類別搜尋結果區塊
+  const docsResults = await page.$('#docsearch-list > div:nth-child(2)');
+
+  // 在文件類別結果中點擊第一個連結
+  const firstDocLink = await docsResults.$('a');
+  await firstDocLink.click();
+
+  // 等待標題元素出現並取得文字內容
+  const titleElement = await page.waitForSelector('h1');
+  const titleText = await page.evaluate(el => el.textContent, titleElement);
+
+  // 印出標題文字
+  console.log(titleText);
+
+  // 關閉瀏覽器
+  await browser.close();
 })();
