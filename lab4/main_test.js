02bda22e2dd79a37174cbbf1cf27d2494b97e106
n19971019 <137515554+n19971019@users.noreply.github.com>
2024-04-15 14:44:11 +0800

    Update main_test.js

--- lab4/main_test.js
+++ lab4/main_test.js
@@ -5,32 +5,31 @@ const puppeteer = require('puppeteer');
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
-  // 前往指定網址
+  // 前往指定的網址
   await page.goto('https://pptr.dev/');
 
-  // 找到搜尋按鈕並點擊
-  const searchButton = await page.$('.DocSearch-Button'); // 使用 class 選擇器
-  await searchButton.click();
+  // 點擊搜尋按鈕
+  const searchButtonSelector = '.DocSearch-Button'; // 使用 CSS 選擇器找到搜尋按鈕
+  await page.waitForSelector(searchButtonSelector); // 等待搜尋按鈕出現
+  await page.click(searchButtonSelector); // 點擊搜尋按鈕
 
-  // 輸入搜尋關鍵字
-  const searchInput = await page.$('#docsearch-input'); // 使用 id 選擇器
-  await searchInput.type('chipi chipi chapa chapa');
+  // 輸入搜尋字串
+  const searchInputSelector = '#docsearch-input'; // 使用 CSS 選擇器找到搜尋輸入框
+  await page.waitForSelector(searchInputSelector); // 等待搜尋輸入框出現
+  await page.type(searchInputSelector, 'chipi chipi chapa chapa'); // 輸入搜尋字串
 
-  // 等待搜尋結果出現
-  await page.waitForSelector('#docsearch-item-5 > a');
+  // 等待搜尋結果出現並點擊 Docs 區塊的第一個結果
+  const firstDocResultSelector = '#docsearch-item-5 > a'; // 使用 CSS 選擇器找到 Docs 區塊的第一個結果
+  await page.waitForSelector(firstDocResultSelector); // 等待 Docs 區塊的第一個結果出現
+  await page.click(firstDocResultSelector); // 點擊 Docs 區塊的第一個結果
 
-  // 點擊文件分類中第一個結果
-  const firstDocResult = await page.$('#docsearch-item-5 > a');
-  await firstDocResult.click();
+  // 取得頁面標題
+  const titleSelector = 'h1'; // 使用 CSS 選擇器找到標題元素
+  await page.waitForSelector(titleSelector); // 等待標題元素出現
+  const titleElement = await page.$(titleSelector); // 取得標題元素
+  const titleText = await page.evaluate(el => el.textContent, titleElement); // 取得標題文字
 
-  // 等待標題元素出現
-  await page.waitForSelector('h1');
-
-  // 取得標題文字
-  const titleElement = await page.$('h1');
-  const titleText = await page.evaluate(el => el.textContent, titleElement);
-
-  // 列印標題
+  // 輸出標題文字
   console.log(titleText);
 
   // 關閉瀏覽器
