48299bc47466022246853cf44f47d4091c38c6d9
n19971019 <137515554+n19971019@users.noreply.github.com>
2024-04-15 14:13:20 +0800

    Update main_test.js

--- lab4/main_test.js
+++ lab4/main_test.js
@@ -1,22 +1,49 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-    // Launch the browser and open a new blank page
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
+  // 啟動瀏覽器並開啟新分頁
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
 
-    // Navigate the page to a URL
+  try {
+    // 前往指定的網站
     await page.goto('https://pptr.dev/');
 
-    // Hints:
-    // Click search button
-    // Type into search box
-    // Wait for search result
-    // Get the `Docs` result section
-    // Click on first result in `Docs` section
-    // Locate the title
-    // Print the title
+    // 找到並點擊搜尋按鈕
+    const searchButton = await page.waitForSelector('.DocSearch-Button');
+    await searchButton.click();
 
-    // Close the browser
+    // 輸入搜尋文字
+    const searchInput = await page.waitForSelector('#docsearch-input');
+    await searchInput.type('chipi chipi chapa chapa');
+
+    // 等待搜尋結果出現
+    await page.waitForSelector('#docsearch-item-5 > a');
+
+    // 定位文件類別的搜尋結果區塊
+    const docsResults = await page.$$('.DocSearch-Dropdown-Container > ul > li');
+
+    // 找到第一個文件類別的搜尋結果並點擊
+    for (const result of docsResults) {
+      const category = await result.$eval('.DocSearch-Hit-category', el => el.textContent);
+      if (category === 'Docs') {
+        await result.click();
+        break;
+      }
+    }
+
+    // 等待頁面標題元素出現
+    const titleElement = await page.waitForSelector('h1');
+
+    // 取得頁面標題文字
+    const titleText = await page.evaluate(el => el.textContent, titleElement);
+
+    // 輸出頁面標題
+    console.log(titleText);
+  } catch (error) {
+    console.error('搜尋過程中發生錯誤:', error);
+  } finally {
+    // 關閉瀏覽器
     await browser.close();
+  }
 })();
