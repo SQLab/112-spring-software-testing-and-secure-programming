0fbe5c1c6348100aa072331b085280659b0507fa
n19971019 <137515554+n19971019@users.noreply.github.com>
2024-04-15 14:33:22 +0800

    Update main_test.js

--- lab4/main_test.js
+++ lab4/main_test.js
@@ -1,42 +1,36 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  // 啟動瀏覽器並開啟新分頁
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+    // 啟動瀏覽器並開啟新分頁
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
 
-  try {
-    // 前往指定網站
-    await page.goto('https://pptr.dev/');
+    try {
+        // 前往指定網址
+        await page.goto('https://pptr.dev/');
 
-    // 等待搜尋框出現並輸入查詢字串
-    const searchInput = await page.waitForSelector('#docsearch-input', { visible: true });
-    await searchInput.type('chipi chipi chapa chapa');
+        // 等待搜索框出現並輸入查詢字串
+        const searchInput = await page.waitForSelector('#docsearch-input');
+        await searchInput.type('chipi chipi chapa chapa');
 
-    // 觸發搜尋事件（模擬按下 Enter 鍵）
-    await page.keyboard.press('Enter');
+        // 等待搜索按鈕出現並點擊
+        const searchButton = await page.waitForSelector('.DocSearch-Button');
+        await searchButton.click();
 
-    // 等待文件搜尋結果出現
-    await page.waitForSelector('#docsearch-item-0');
+        // 等待 Docs 區塊的第一個結果出現並點擊
+        const firstResult = await page.waitForSelector('#docsearch-item-5 > a');
+        await firstResult.click();
 
-    // 取得第一個文件搜尋結果的連結元素
-    const firstDocResult = await page.$('#docsearch-item-0 a');
+        // 等待頁面標題出現並取得文字內容
+        const titleElement = await page.waitForSelector('h1');
+        const titleText = await page.evaluate(el => el.textContent, titleElement);
 
-    // 點擊第一個文件搜尋結果
-    await firstDocResult.click();
-
-    // 等待頁面標題元素出現
-    await page.waitForSelector('h1');
-
-    // 取得頁面標題文字
-    const title = await page.$eval('h1', h1 => h1.textContent);
-
-    // 輸出頁面標題
-    console.log(title);
-  } catch (error) {
-    console.error('搜尋過程中發生錯誤：', error);
-  } finally {
-    // 關閉瀏覽器
-    await browser.close();
-  }
+        // 輸出頁面標題
+        console.log(titleText);
+    } catch (error) {
+        console.error('操作過程中發生錯誤:', error);
+    } finally {
+        // 關閉瀏覽器
+        await browser.close();
+    }
 })();
