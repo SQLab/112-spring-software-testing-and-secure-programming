107e6b6a8e04da937f4d9b84f12c3be151e64eff
n19971019 <137515554+n19971019@users.noreply.github.com>
2024-04-15 14:16:59 +0800

    Update main_test.js

--- lab4/main_test.js
+++ lab4/main_test.js
@@ -5,45 +5,34 @@ const puppeteer = require('puppeteer');
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
-  try {
-    // 前往指定的網站
-    await page.goto('https://pptr.dev/');
+  // 前往指定網址
+  await page.goto('https://pptr.dev/');
 
-    // 找到並點擊搜尋按鈕
-    const searchButton = await page.waitForSelector('.DocSearch-Button');
-    await searchButton.click();
+  // 找到並點擊搜尋按鈕
+  const searchButton = await page.$('.DocSearch-Button'); // 使用 CSS 選擇器
+  await searchButton.click();
 
-    // 輸入搜尋文字
-    const searchInput = await page.waitForSelector('#docsearch-input');
-    await searchInput.type('chipi chipi chapa chapa');
+  // 輸入搜尋文字
+  const searchInput = await page.$('#docsearch-input'); // 使用 CSS 選擇器
+  await searchInput.type('chipi chipi chapa chapa');
 
-    // 等待搜尋結果出現
-    await page.waitForSelector('#docsearch-item-5 > a');
+  // 等待搜尋結果出現
+  await page.waitForSelector('#docsearch-item-5 > a');
 
-    // 定位文件類別的搜尋結果區塊
-    const docsResults = await page.$$('.DocSearch-Dropdown-Container > ul > li');
+  // 取得文件類別搜尋結果區塊
+  const docsResults = await page.$('#docsearch-list > div:nth-child(2)');
 
-    // 找到第一個文件類別的搜尋結果並點擊
-    for (const result of docsResults) {
-      const category = await result.$eval('.DocSearch-Hit-category', el => el.textContent);
-      if (category === 'Docs') {
-        await result.click();
-        break;
-      }
-    }
+  // 在文件類別結果中點擊第一個連結
+  const firstDocLink = await docsResults.$('a');
+  await firstDocLink.click();
 
-    // 等待頁面標題元素出現
-    const titleElement = await page.waitForSelector('h1');
+  // 等待標題元素出現並取得文字內容
+  const titleElement = await page.waitForSelector('h1');
+  const titleText = await page.evaluate(el => el.textContent, titleElement);
 
-    // 取得頁面標題文字
-    const titleText = await page.evaluate(el => el.textContent, titleElement);
+  // 印出標題文字
+  console.log(titleText);
 
-    // 輸出頁面標題
-    console.log(titleText);
-  } catch (error) {
-    console.error('搜尋過程中發生錯誤:', error);
-  } finally {
-    // 關閉瀏覽器
-    await browser.close();
-  }
+  // 關閉瀏覽器
+  await browser.close();
 })();
