bff1eaeab4fdf4e3b8f9cb49128308a93ba7d969
TAO KUO HUA <131828636+agtfsfhgsdf@users.noreply.github.com>
2024-04-18 00:35:46 +0800

    Update main_test.js

--- lab4/main_test.js
+++ lab4/main_test.js
@@ -4,22 +4,22 @@ const puppeteer = require('puppeteer');
     // Launch the browser and open a new blank page
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-
     // Navigate the page to a URL
     await page.goto('https://pptr.dev/');
-
-
-    await page.setViewport({width:1080 , height:1024 });
-    const searchResultSelector = '.DocSearch-Button';
-    await page.waitForSelector(searchResultSelector);
-    await page.click(searchResultSelector);
-    await page.waitForSelector('.DocSearch-Input'); 
-    await page.type('.DocSearch-Input', 'chipi chipi chapa chapa', {delay: 100});
-    await page.waitForSelector('#docsearch-item-5');
-    await page.click('#docsearch-item-5');
-    const titleSelector = await page.waitForSelector('h1');
-    const fullTitle = await titleSelector?.evaluate(el => el.textContent);
-    console.log(fullTitle);
+    
+    await page.click('.DocSearch.DocSearch-Button');
+    await page.waitForSelector('.DocSearch-Input');
+    await page.type('.DocSearch-Input', 'chipi chipi chapa chapa');
+    
+    await new Promise(r => setTimeout(r,1000))
+    await page.waitForSelector('.DocSearch-Hit#docsearch-item-5');
+    await page.click('.DocSearch-Hit#docsearch-item-5');
+    
+    await page.waitForSelector('h1');
+    const text = await page.evaluate(() => {
+      return document.querySelector('h1').innerText;
+    });
+    console.log(text)
 
     // Close the browser
     await browser.close();
