6c715cc3c785bbfa6e07cd74fe9ac6a1c76584f8
ecs7723158 <ecs7723158@gmail.com>
2024-04-17 23:27:59 +0800

    Update main_test.js

--- lab3/main_test.js
+++ lab3/main_test.js
@@ -1,40 +1,37 @@
 const assert = require('assert');
-const Calculator = require('./main').Calculator;
+const { Calculator } = require('./main');
 
+describe("Calculator", () => {
+    const cal = new Calculator();
 
-describe("Calculator", function () {
-    describe("exp", function () {
-        const cal = new Calculator();
-
-        it("should throw an error for unsupported operand type", function () {
-            assert.throws(function () { cal.exp('a') }, Error, 'unsupported operand type');
+    describe("exp", () => {
+        it("should throw an error for unsupported operand type", () => {
+            assert.throws(() => cal.exp('a'), Error, 'unsupported operand type');
         });
 
-        it("should throw an error for overflow", function () {
-            assert.throws(function () { cal.exp(1000) }, Error, 'overflow');
+        it("should throw an error for overflow", () => {
+            assert.throws(() => cal.exp(1000), Error, 'overflow');
         });
 
-        it("should return 1 for input 0", function () {
+        it("should return 1 for input 0", () => {
             assert.strictEqual(cal.exp(0), 1);
         });
     });
 
-    describe("log", function () {
-        const cal = new Calculator();
-
-        it("should throw an error for unsupported operand type", function () {
-            assert.throws(function () { cal.log('a') }, Error, 'unsupported operand type');
+    describe("log", () => {
+        it("should throw an error for unsupported operand type", () => {
+            assert.throws(() => cal.log('a'), Error, 'unsupported operand type');
         });
 
-        it("should throw an error for math domain error (1)", function () {
-            assert.throws(function () { cal.log(0) }, Error, 'math domain error (1)');
+        it("should throw an error for math domain error (1)", () => {
+            assert.throws(() => cal.log(0), Error, 'math domain error (1)');
         });
 
-        it("should throw an error for math domain error (2)", function () {
-            assert.throws(function () { cal.log(-1) }, Error, 'math domain error (2)');
+        it("should throw an error for math domain error (2)", () => {
+            assert.throws(() => cal.log(-1), Error, 'math domain error (2)');
         });
 
-        it("should return 0 for input 1", function () {
+        it("should return 0 for input 1", () => {
             assert.strictEqual(cal.log(1), 0);
         });
     });
