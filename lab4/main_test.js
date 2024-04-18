0997cf760627ed70f90a74aceed83640b497a702
n19971019 <137515554+n19971019@users.noreply.github.com>
2024-04-18 09:19:43 +0800

    Update main_test.js

--- lab4/main_test.js
+++ lab4/main_test.js
@@ -1,40 +1,25 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-    // 啟動瀏覽器並開啟新頁面
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
+  // 啟動瀏覽器並開啟新分頁
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
 
-    // 前往指定的網址
-    await page.goto('https://pptr.dev/');
+  // 前往指定網址
+  await page.goto('https://pptr.dev/');
 
-    // 方法一：使用CSS選擇器
-    // 點擊搜尋框
-    await page.click('#search');
+  // 輸入搜尋字詞並等待結果
+  await page.type('#search', 'chipi chipi chapa chapa');
+  await page.waitForSelector('.DocSearch-Hit-source');
 
-    // 輸入搜尋字串
-    await page.type('#search', 'chipi chipi chapa chapa');
+  // 取得文件區塊的第一個結果並點擊
+  const docsResult = await page.$('.DocSearch-Hit-source');
+  await docsResult.click();
 
-    // 等待搜尋結果出現
-    await page.waitForSelector('.DocSearch-Hit-source');
+  // 獲取標題並印出
+  const title = await page.title();
+  console.log(title);
 
-    // 取得所有文件搜尋結果
-    const docsResults = await page.$$('.DocSearch-Hit-source');
-
-    // 點擊第一個文件搜尋結果
-    await docsResults[0].click();
-
-    // 取得標題元素
-    const titleElement = await page.$('.postHeaderTitle');
-    const title = await page.evaluate(el => el.textContent, titleElement);
-
-    // 顯示標題
-    console.log(title);
-
-    // 方法二：使用XPath選擇器
-    // //*[@id="__docusaurus"]//input[@type="search"]
-    // //*[@id="__docusaurus"]//article[1]//h1
-
-    // 關閉瀏覽器
-    await browser.close();
+  // 關閉瀏覽器
+  await browser.close();
 })();
