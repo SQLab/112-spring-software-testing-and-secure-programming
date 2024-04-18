0b364569f1e80bf269ccd61c4559816b55bc15f9
n19971019 <137515554+n19971019@users.noreply.github.com>
2024-04-18 13:30:21 +0800

    Update main_test.js

--- lab4/main_test.js
+++ lab4/main_test.js
@@ -1,39 +1,32 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-    // 啟動瀏覽器並開啟一個新的空白頁面
-    browser = await puppeteer.launch();
+    // 啟動瀏覽器並開啟新分頁
+    const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
-    // 導航頁面到指定的 URL
+    // 前往指定的網址
     await page.goto('https://pptr.dev/');
 
-    // 找到搜索框並輸入 "chipi chipi chapa chapa"
-    const searchBox = await page.$('input[type="search"]'); 
-    await searchBox.type('chipi chipi chapa chapa');
+    // 輸入搜尋文字
+    await page.type('#search-box input[type="search"]', 'chipi chipi chapa chapa');
 
-    // 點擊搜索按鈕 (Alternative: 按下 Enter 鍵)
-    // await page.click('button[type="submit"]'); // 原本的點擊方法
-    await searchBox.press('Enter'); // 使用 Enter 鍵進行搜索
+    // 使用 XPath 定位搜尋按鈕並點擊
+    const searchButton = await page.$x('//*[@id="search-box"]/button');
+    await searchButton[0].click();
 
-    // 等待搜索結果出現
+    // 等待搜尋結果出現
     await page.waitForSelector('.DocSearch-Hit-source');
 
-    // 找到 "Docs" 結果區塊
-    const docsSection = await page.$('.DocSearch-Hit-source:nth-child(1)'); 
+    // 使用 XPath 定位第一個 Docs 搜尋結果並點擊
+    const firstDocResult = await page.$x('//*[@class="DocSearch-Hit-source"][1]/a');
+    await firstDocResult[0].click();
 
-    // 點擊 "Docs" 區塊中的第一個結果
-    const firstResult = await docsSection.$('a');
-    await firstResult.click();
+    // 使用 XPath 定位標題元素並取得標題文字
+    const titleElement = await page.$x('//*[@class="postHeaderTitle-root"]/span');
+    const title = await page.evaluate(element => element.textContent, titleElement[0]);
 
-    // 等待頁面加载完成
-    await page.waitForSelector('h1');
-
-    // 找到標題元素並提取標題文字
-    const titleElement = await page.$('h1');
-    const title = await page.evaluate(el => el.textContent, titleElement);
-
-    // 打印標題
+    // 輸出標題
     console.log(title);
 
     // 關閉瀏覽器
