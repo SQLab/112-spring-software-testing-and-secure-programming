e3d672e025e3464a764f54d5212fe345830cfd2b
n19971019 <137515554+n19971019@users.noreply.github.com>
2024-04-18 14:07:12 +0800

    Update main_test.js

--- lab4/main_test.js
+++ lab4/main_test.js
@@ -8,31 +8,27 @@ const puppeteer = require('puppeteer');
     // 導航到指定的網站
     await page.goto('https://pptr.dev/');
 
-    // 點擊搜尋按鈕 (Alternative solution using XPath)
-    const searchButtonXPath = '//*[@class="DocSearch-Button"]';
-    const searchButtonElement = await page.$x(searchButtonXPath);
-    await searchButtonElement[0].click();
+    // 1. 點擊搜尋按鈕 (使用 CSS 選擇器)
+    await page.waitForSelector('.DocSearch-Button'); // 等待搜尋按鈕出現
+    await page.click('.DocSearch-Button'); // 點擊搜尋按鈕
 
-    // 輸入搜尋查詢字串
-    const searchInputSelector = '#docsearch-input';
-    await page.waitForSelector(searchInputSelector);
-    await page.type(searchInputSelector, 'chipi chipi chapa chapa');
+    // 2. 輸入搜尋查詢字串 (使用 CSS 選擇器)
+    await page.waitForSelector('.DocSearch-Input'); // 等待搜尋輸入框出現
+    await page.type('.DocSearch-Input', 'chipi chipi chapa chapa'); // 輸入查詢字串
 
-    // 等待搜尋結果並點擊文件區塊中的第一個結果 (Alternative solution using page.evaluate)
-    await page.waitForSelector('#docsearch-item-5 > a');
-    await page.evaluate(() => {
-        document.querySelector('#docsearch-item-5 > a').click();
-    });
+    // 3. 等待搜尋結果並點擊文件區塊中的第一個結果 (使用 CSS 選擇器)
+    await page.waitForSelector('.DocSearch-Hit-source'); // 等待文件結果區塊出現
+    const firstResult = await page.$('.DocSearch-Hit-source'); // 選擇第一個文件結果
+    await firstResult.click(); // 點擊第一個文件結果
 
-    // 取得頁面的標題
-    const titleSelector = 'h1';
-    await page.waitForSelector(titleSelector);
-    const titleElement = await page.$(titleSelector);
-    const titleText = await page.evaluate(el => el.textContent, titleElement);
+    // 4. 取得頁面的標題 (使用 CSS 選擇器)
+    await page.waitForSelector('h1'); // 等待標題元素出現
+    const titleElement = await page.$('h1'); // 選擇標題元素
+    const titleText = await page.evaluate(el => el.textContent, titleElement); // 取得標題文字
 
-    // 列印標題
+    // 5. 列印標題
     console.log(titleText);
 
-    // 關閉瀏覽器
+    // 6. 關閉瀏覽器
     await browser.close();
 })();
