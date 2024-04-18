b7cf73297313d68b34acc3f43a0161443665e42c
n19971019 <137515554+n19971019@users.noreply.github.com>
2024-04-18 09:15:17 +0800

    Update main_test.js

--- lab4/main_test.js
+++ lab4/main_test.js
@@ -1,41 +1,40 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  // 啟動瀏覽器並開啟新分頁
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+    // 啟動瀏覽器並開啟新頁面
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
 
-  // 前往指定的網址
-  await page.goto('https://pptr.dev/');
+    // 前往指定的網址
+    await page.goto('https://pptr.dev/');
 
-  // 找到搜尋框並輸入 "chipi chipi chapa chapa"
-  const searchBox = await page.$('#search'); // 使用 CSS 選擇器找到搜尋框
-  await searchBox.type('chipi chipi chapa chapa');
+    // 方法一：使用CSS選擇器
+    // 點擊搜尋框
+    await page.click('#search');
 
-  // 點擊搜尋按鈕
-  const searchButton = await page.$('.DocSearch-Button'); // 使用 CSS 選擇器找到搜尋按鈕
-  await searchButton.click();
+    // 輸入搜尋字串
+    await page.type('#search', 'chipi chipi chapa chapa');
 
-  // 等待搜尋結果出現
-  await page.waitForSelector('.DocSearch-Hit-source');
+    // 等待搜尋結果出現
+    await page.waitForSelector('.DocSearch-Hit-source');
 
-  // 取得 "Docs" 結果區塊
-  const docsSection = await page.$('.DocSearch-Hit-source');
+    // 取得所有文件搜尋結果
+    const docsResults = await page.$$('.DocSearch-Hit-source');
 
-  // 點擊 "Docs" 區塊中的第一個結果
-  const firstResult = await docsSection.$('a');
-  await firstResult.click();
+    // 點擊第一個文件搜尋結果
+    await docsResults[0].click();
 
-  // 等待頁面載入完成
-  await page.waitForSelector('h1');
+    // 取得標題元素
+    const titleElement = await page.$('.postHeaderTitle');
+    const title = await page.evaluate(el => el.textContent, titleElement);
 
-  // 取得標題
-  const titleElement = await page.$('h1'); // 使用 CSS 選擇器找到標題元素
-  const title = await page.evaluate(el => el.textContent, titleElement);
+    // 顯示標題
+    console.log(title);
 
-  // 輸出標題
-  console.log(title);
+    // 方法二：使用XPath選擇器
+    // //*[@id="__docusaurus"]//input[@type="search"]
+    // //*[@id="__docusaurus"]//article[1]//h1
 
-  // 關閉瀏覽器
-  await browser.close();
+    // 關閉瀏覽器
+    await browser.close();
 })();
