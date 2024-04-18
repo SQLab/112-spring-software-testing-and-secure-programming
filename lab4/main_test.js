ca5beae865d8ff4f1cc57600ed5cd7d44743e489
n19971019 <137515554+n19971019@users.noreply.github.com>
2024-04-18 14:04:12 +0800

    Update main_test.js

--- lab4/main_test.js
+++ lab4/main_test.js
@@ -1,38 +1,38 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  // 啟動瀏覽器並開啟一個新的空白頁面
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+    // 啟動瀏覽器並開啟一個新的空白頁面
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
 
-  // 導航到指定的網站
-  await page.goto('https://pptr.dev/');
+    // 導航到指定的網站
+    await page.goto('https://pptr.dev/');
 
-  // 搜尋並點擊結果
+    // 點擊搜尋按鈕 (Alternative solution using XPath)
+    const searchButtonXPath = '//*[@class="DocSearch-Button"]';
+    const searchButtonElement = await page.$x(searchButtonXPath);
+    await searchButtonElement[0].click();
 
-  // 1. 使用 XPath 定位搜尋按鈕並點擊
-  const searchButtonXPath = '//*[@class="DocSearch-Button"]';
-  const searchButton = await page.$x(searchButtonXPath);
-  await searchButton[0].click();
+    // 輸入搜尋查詢字串
+    const searchInputSelector = '#docsearch-input';
+    await page.waitForSelector(searchInputSelector);
+    await page.type(searchInputSelector, 'chipi chipi chapa chapa');
 
-  // 2. 使用 XPath 定位搜尋輸入框並輸入文字
-  const searchInputXPath = '//*[@class="DocSearch-Input"]';
-  const searchInput = await page.$x(searchInputXPath);
-  await searchInput[0].type('chipi chipi chapa chapa');
+    // 等待搜尋結果並點擊文件區塊中的第一個結果 (Alternative solution using page.evaluate)
+    await page.waitForSelector('#docsearch-item-5 > a');
+    await page.evaluate(() => {
+        document.querySelector('#docsearch-item-5 > a').click();
+    });
 
-  // 3. 等待搜尋結果出現，並使用 CSS 選擇器點擊第一個結果
-  await page.waitForSelector('#docsearch-item-0 > a');
-  const firstResult = await page.$('#docsearch-item-0 > a');
-  await firstResult.click();
+    // 取得頁面的標題
+    const titleSelector = 'h1';
+    await page.waitForSelector(titleSelector);
+    const titleElement = await page.$(titleSelector);
+    const titleText = await page.evaluate(el => el.textContent, titleElement);
 
-  // 4. 使用 CSS 選擇器定位標題並取得文字
-  await page.waitForSelector('h1');
-  const titleElement = await page.$('h1');
-  const titleText = await page.evaluate(el => el.textContent, titleElement);
+    // 列印標題
+    console.log(titleText);
 
-  // 5. 列印標題
-  console.log(titleText);
-
-  // 關閉瀏覽器
-  await browser.close();
+    // 關閉瀏覽器
+    await browser.close();
 })();
