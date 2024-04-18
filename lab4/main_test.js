fcccd9bf6110c451b235de52f11b9ef16ba6aacd
n19971019 <137515554+n19971019@users.noreply.github.com>
2024-04-18 14:47:17 +0800

    Update main_test.js

--- lab4/main_test.js
+++ lab4/main_test.js
@@ -1,34 +1,37 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-    // 啟動瀏覽器並開啟一個新的空白頁面
+    // 啟動瀏覽器並開啟新分頁
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
-    // 導航到指定的網站
+    // 前往指定的網址
     await page.goto('https://pptr.dev/');
 
-    // 1. 點擊搜尋按鈕 (使用 CSS 選擇器)
-    await page.waitForSelector('.DocSearch-Button'); // 等待搜尋按鈕出現
-    await page.click('.DocSearch-Button'); // 點擊搜尋按鈕
+    // 等待搜尋按鈕出現並點擊
+    const searchButtonSelector = '.DocSearch-Button';
+    await page.waitForSelector(searchButtonSelector);
+    await page.click(searchButtonSelector);
 
-    // 2. 輸入搜尋查詢字串 (使用 CSS 選擇器)
-    await page.waitForSelector('.DocSearch-Input'); // 等待搜尋輸入框出現
-    await page.type('.DocSearch-Input', 'chipi chipi chapa chapa'); // 輸入查詢字串
+    // 等待搜尋輸入框出現並輸入查詢字串
+    const searchInputSelector = '#docsearch-input';
+    await page.waitForSelector(searchInputSelector);
+    await page.type(searchInputSelector, 'chipi chipi chapa chapa');
 
-    // 3. 等待搜尋結果並點擊文件區塊中的第一個結果 (使用 CSS 選擇器)
-    await page.waitForSelector('.DocSearch-Hit-source'); // 等待文件結果區塊出現
-    const firstResult = await page.$('.DocSearch-Hit-source'); // 選擇第一個文件結果
-    await firstResult.click(); // 點擊第一個文件結果
+    // 等待第一個文件結果出現並點擊
+    const firstDocResultSelector = '#docsearch-item-5 > a';
+    await page.waitForSelector(firstDocResultSelector);
+    await page.click(firstDocResultSelector);
 
-    // 4. 取得頁面的標題 (使用 CSS 選擇器)
-    await page.waitForSelector('h1'); // 等待標題元素出現
-    const titleElement = await page.$('h1'); // 選擇標題元素
-    const titleText = await page.evaluate(el => el.textContent, titleElement); // 取得標題文字
+    // 等待標題元素出現並取得標題文字
+    const titleSelector = 'h1';
+    await page.waitForSelector(titleSelector);
+    const titleElement = await page.$(titleSelector);
+    const titleText = await page.evaluate(el => el.textContent, titleElement);
 
-    // 5. 列印標題
+    // 印出標題
     console.log(titleText);
 
-    // 6. 關閉瀏覽器
+    // 關閉瀏覽器
     await browser.close();
 })();
