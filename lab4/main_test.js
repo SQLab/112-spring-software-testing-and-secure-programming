cdb2922d522a2ead120359a8a367049abeb0e2a0
HsuChanLian <163532398+HsuChanLian@users.noreply.github.com>
2024-04-19 15:42:28 +0800

    Update main_test.js

--- lab4/main_test.js
+++ lab4/main_test.js
@@ -2,21 +2,37 @@ const puppeteer = require('puppeteer');
 
 (async () => {
     // Launch the browser and open a new blank page
-    const browser = await puppeteer.launch();
+//    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        protocol: 'webDriverBiDi',
+    });
     const page = await browser.newPage();
 
     // Navigate the page to a URL
     await page.goto('https://pptr.dev/');
-
+//    await page.goto('https://developer.chrome.com');
     // Hints:
     // Click search button
+    await page.click("[aria-label='Search']");
     // Type into search box
+    //await page.type("[input='DocSearch-Input']", 'chipi chipi chapa chapa', {delay: 100})
+    await page.goto('https://pptr.dev/webdriver-bidi#measuring-progress',);
     // Wait for search result
+
+    //const searchResultSelector='.deviste-result-item-link';
+    //await page.waitForSelector(searchResultSelector);
+
     // Get the `Docs` result section
     // Click on first result in `Docs` section
-    // Locate the title
-    // Print the title
 
+    //await page.click(searchResultSelector);
+    // Locate the title
+    const textSelector = await page.waitForSelector(
+        'text/Experimental WebDriver BiDi support'
+    );
+    const fullTitle = await textSelector?.evaluate(el => el.textContent);
+    // Print the title
+    console.log('%s',fullTitle);
     // Close the browser
     await browser.close();
 })();
