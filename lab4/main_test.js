dc9a38f0a3bf10895b8fdbe9e0d69e7beaab6082
TAO KUO HUA <131828636+agtfsfhgsdf@users.noreply.github.com>
2024-04-19 10:45:40 +0800

    Update main_test.js

--- lab4/main_test.js
+++ lab4/main_test.js
@@ -1,39 +1,18 @@
 const puppeteer = require('puppeteer');
-
 (async () => {
-
-    // open browser
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-
-    await page.goto('https://pptr.dev/webdriver-bidi/#measuring-progress');
-
-    await page.waitForSelector('.DocSearch-Button-Placeholder'); 
-    await page.click('.DocSearch-Button-Placeholder'); 
-    // wait
-    
+    await page.goto('https://pptr.dev/');
+    await page.setViewport({width:1080 , height:1024 });
+    const searchResultSelector = '.DocSearch-Button';
+    await page.waitForSelector(searchResultSelector);
+    await page.click(searchResultSelector);
     await page.waitForSelector('.DocSearch-Input'); 
-    await page.type('.DocSearch-Input', 'chipi chipi chapa chapa'); //input
-    await page.waitForSelector('.DocSearch-Hits')
-    
-    await page.waitForFunction(() => {
-        
-        const hit = Array.from(document.querySelectorAll('.DocSearch-Hit-title')).find(e => e.textContent.includes('To gauge the capabilities of'));
-        return hit && hit.closest('.DocSearch-Hit-Container');
-    });
-
-    await page.evaluate(() => {
-        const hit = Array.from(document.querySelectorAll('.DocSearch-Hit-title')).find(e => e.textContent.includes('To gauge the capabilities of'));
-        hit && hit.closest('.DocSearch-Hit-Container').click();
-    });
-
-
-    await page.waitForSelector('h1', { visible: true });
-    
-    // output
-    const headerText = await page.$eval('h1', element => element.textContent);
-    console.log(headerText);  
-
-    await browser.close();
-
+    await page.type('.DocSearch-Input', 'chipi chipi chapa chapa', {delay: 100});
+    await page.waitForSelector('#docsearch-item-5');
+    await page.click('#docsearch-item-5');
+    const titleSelector = await page.waitForSelector('h1');
+    const fullTitle = await titleSelector?.evaluate(el => el.textContent);
+    console.log(fullTitle);
+    await browser.close();    
 })();
