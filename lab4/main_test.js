9c49b96df9ad460b1127bb04ab1136da8de6b98e
Oliver <162861141+Oliver84586454@users.noreply.github.com>
2024-04-22 20:22:19 +0800

    Update main_test.js

--- lab4/main_test.js
+++ lab4/main_test.js
@@ -1,22 +1,38 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-    // Launch the browser and open a new blank page
+
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
-    // Navigate the page to a URL
+
     await page.goto('https://pptr.dev/');
 
-    // Hints:
-    // Click search button
-    // Type into search box
-    // Wait for search result
-    // Get the `Docs` result section
-    // Click on first result in `Docs` section
-    // Locate the title
-    // Print the title
 
-    // Close the browser
+    await page.setViewport({width: 1080, height: 1024});
+
+    const searchButtonSelector = '.DocSearch-Button';
+    await page.waitForSelector(searchButtonSelector);
+    await page.click(searchButtonSelector);
+    
+
+    const searchInputSelector = '#docsearch-input';
+    await page.waitForSelector(searchInputSelector);
+    await page.type(searchInputSelector, 'chipi chipi chapa chapa');
+    
+
+    await new Promise(r => setTimeout(r,1000))
+    const searchResultSelector = '#docsearch-item-5 a';
+    await page.waitForSelector(searchResultSelector);
+    await page.click(searchResultSelector);
+
+    const titleSelector = 'h1';
+    const titleElement = await page.waitForSelector(titleSelector);
+    const element = await page.$(titleSelector);
+    const title = await titleElement.evaluate(el => el.textContent);
+
+
+    console.log(title);
+
     await browser.close();
 })();
