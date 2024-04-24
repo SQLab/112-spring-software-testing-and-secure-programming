9f5f9f427e188411f9949238ce6ff410f4e7a846
liujayo <37663339+toey8612@users.noreply.github.com>
2024-04-24 11:27:40 +0800

    Update main_test.js

--- lab4/main_test.js
+++ lab4/main_test.js
@@ -1,22 +1,44 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-    // Launch the browser and open a new blank page
+    // 啟動瀏覽器並打開一個新的空白頁面
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
+    try{
+        // 導航至指定網址
+        await page.goto('https://pptr.dev/');
+        await new Promise(resolve => setTimeout(resolve, 1000));
 
-    // Navigate the page to a URL
-    await page.goto('https://pptr.dev/');
+        // 點擊搜尋按鈕
+        await page.click('button.DocSearch.DocSearch-Button');
+        await new Promise(resolve => setTimeout(resolve, 1000));
 
-    // Hints:
-    // Click search button
-    // Type into search box
-    // Wait for search result
-    // Get the `Docs` result section
-    // Click on first result in `Docs` section
-    // Locate the title
-    // Print the title
+        // 在搜尋框中輸入文字
+        await page.waitForSelector('#docsearch-input');
+        await page.type('#docsearch-input', 'chipi chipi chapa chapa', { delay: 1000 });
+        await new Promise(resolve => setTimeout(resolve, 1000));
 
-    // Close the browser
-    await browser.close();
+        // 等待搜尋結果
+        await page.waitForSelector('#docsearch-item-5');
+        await new Promise(resolve => setTimeout(resolve, 1000));
+
+        // 點擊「Docs」部分的第一個結果
+        await page.click('#docsearch-item-5');
+        await new Promise(resolve => setTimeout(resolve, 1000));
+
+        // 定位標題
+        await page.waitForSelector('h1');
+
+        // 獲取標題元素的文字內容
+        const title = await page.evaluate(() => {
+            const titleElement = document.querySelector('h1');
+            return titleElement.textContent;
+        });
+        console.log(title);
+        await browser.close();
+    }catch(error){
+        console.error('An error occurred:', error);
+    }finally{
+        // 最後都會關閉瀏覽器
+        await browser.close();}
 })();
