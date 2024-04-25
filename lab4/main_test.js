24f419f3e27032cf5a098e71ccc07eea1aed0b21
CHIH MING CHEN <129422182+Yyrff@users.noreply.github.com>
2024-04-25 12:02:38 +0800

    Update main_test.js

--- lab4/main_test.js
+++ lab4/main_test.js
@@ -7,16 +7,29 @@ const puppeteer = require('puppeteer');
 
     // Navigate the page to a URL
     await page.goto('https://pptr.dev/');
-
+    
     // Hints:
     // Click search button
+    await page.click('button.DocSearch.DocSearch-Button'); 
+    
     // Type into search box
     // Wait for search result
-    // Get the `Docs` result section
-    // Click on first result in `Docs` section
-    // Locate the title
-    // Print the title
+    await page.waitForSelector('#docsearch-input.DocSearch-Input');
+    await page.type('#docsearch-input.DocSearch-Input', 'chipi chipi chapa chapa',{delay: 2000}); 
 
+    
+    // Get the `Docs` result section
+    await page.waitForSelector('#docsearch-item-5.DocSearch-Hit'); 
+    // Click on first result in `Docs` section
+    await page.click('#docsearch-item-5.DocSearch-Hit');
+    
+    // Locate the title
+    let titletext = await page.waitForSelector('#__docusaurus_skipToContent_fallback > div > div > main > div > div > div > div > article > div.theme-doc-markdown.markdown > h1'); 
+    let flag = await titletext.evaluate(element=> element.textContent);
+    
+    // Print the title
+    console.log(flag); 
+    
     // Close the browser
     await browser.close();
 })();
