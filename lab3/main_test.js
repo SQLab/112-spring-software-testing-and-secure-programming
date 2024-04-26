50f63248aef0db62bfbbf41e67c87d4caa386191
CHIH MING CHEN <129422182+Yyrff@users.noreply.github.com>
2024-04-26 12:55:05 +0800

    Update main_test.js

--- lab3/main_test.js
+++ lab3/main_test.js
@@ -2,4 +2,52 @@ const { describe, it } = require('node:test');
 const assert = require('assert');
 const { Calculator } = require('./main');
 
-// TODO: write your tests here
+describe('Calculator', function() {
+    describe('exp()', function() {
+        
+        const testCases = [
+            { input: 2, expected: Math.exp(2) },
+            { input: 1, expected: Math.exp(1) },
+            { input: 0, expected: Math.exp(0) },
+        ];
+        testCases.forEach((testCase) => {
+            it(`should return correct value for input ${testCase.input}`, function() {
+                let calculator = new Calculator();
+                assert.strictEqual(calculator.exp(testCase.input), testCase.expected);
+            });
+        });
+
+        
+        const errorCases = [Infinity, -Infinity, NaN, 1000];
+        errorCases.forEach((errorCase) => {
+            it(`should throw error for input ${errorCase}`, function() {
+                let calculator = new Calculator();
+                assert.throws(() => calculator.exp(errorCase), Error);
+            });
+        });
+    });
+
+    describe('log()', function() {
+        
+        const testCases = [
+            { input: 2, expected: Math.log(2) },
+            { input: 1, expected: Math.log(1) },
+            { input: Math.E, expected: Math.log(Math.E) },
+        ];
+        testCases.forEach((testCase) => {
+            it(`should return correct value for input ${testCase.input}`, function() {
+                let calculator = new Calculator();
+                assert.strictEqual(calculator.log(testCase.input), testCase.expected);
+            });
+        });
+
+      
+        const errorCases = [Infinity, -Infinity, NaN, -1, 0];
+        errorCases.forEach((errorCase) => {
+            it(`should throw error for input ${errorCase}`, function() {
+                let calculator = new Calculator();
+                assert.throws(() => calculator.log(errorCase), Error);
+            });
+        });
+    });
+});
