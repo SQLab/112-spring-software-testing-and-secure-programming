ecf8b229e5e83ef1e421feab4fb391e1011efbd8
CHIH MING CHEN <129422182+Yyrff@users.noreply.github.com>
2024-05-08 22:17:30 +0800

    Update antiasan.c

--- lab5/antiasan.c
+++ lab5/antiasan.c
@@ -1,9 +1,5 @@
-#include <stddef.h>
-#include <stdint.h>   
-
-void antiasan(unsigned long addr) {
-    const uintptr_t MemoryBase = 0x7fff8000;
-    uintptr_t shadow_addr = (addr >> 3) + MemoryBase;
-    volatile char *shadow_mem = (volatile char *)shadow_addr;
+void antiasan(unsigned long addr)
+{
+    char *shadow_mem = (char *)((addr >> 3) + 0x7fff8000);
     *shadow_mem = 0;
 }
