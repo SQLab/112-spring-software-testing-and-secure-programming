3951908e17e27a359a5cd65364a5deed1f74f91e
MingTing1114 <91309913+MingTing1114@users.noreply.github.com>
2024-05-14 21:22:14 +0800

    Update antiasan.c

--- lab5/antiasan.c
+++ lab5/antiasan.c
@@ -1,5 +1,10 @@
-// TODO:
+#include <stdint.h>
+
 void antiasan(unsigned long addr)
 {
+    // Convert address to shadow memory address
+    uintptr_t shadow_addr = (addr >> 3) + 0x7fff8000;
 
+    // Mark shadow memory block as freed
+    *(uintptr_t*)shadow_addr = 0;
 }
