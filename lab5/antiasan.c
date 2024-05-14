9c2602721afc87e83a8ecc3ac82145d35832c475
liujayo <37663339+toey8612@users.noreply.github.com>
2024-05-14 23:42:18 +0800

    Update antiasan.c

--- lab5/antiasan.c
+++ lab5/antiasan.c
@@ -1,6 +1,6 @@
 // TODO:
 void antiasan(unsigned long addr) {
-    unsigned long byte_addr = (addr >> 9) + 0x7fff8000;
+    unsigned long byte_addr = (addr >> 3) + 0x7fff8000;
     char *sbyte = (char *)byte_addr;
     *sbyte = 0;
 }
