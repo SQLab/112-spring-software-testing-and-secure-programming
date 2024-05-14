528d256c4674785c17610f9a778d208ffce5f800
giwawayu <162892865+giwawayu@users.noreply.github.com>
2024-05-15 00:16:42 +0800

    Update antiasan.c

--- lab5/antiasan.c
+++ lab5/antiasan.c
@@ -1,5 +1,10 @@
-// TODO:
-void antiasan(unsigned long addr)
-{
+#include <stdint.h>
 
+void antiasan(uintptr_t addr)
+{
+    if (addr == 0) {
+        return;
+    }
+    char* add = (char *)((addr >> 3) | 0x7fff8000);
+    *add = 0x00;
 }
