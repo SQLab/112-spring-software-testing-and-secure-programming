c31614ee42c570dae65176397b3a4021401dee81
chihchia0707 <162073881+chihchia0707@users.noreply.github.com>
2024-05-14 21:30:06 +0800

    Update antiasan.c

--- lab5/antiasan.c
+++ lab5/antiasan.c
@@ -1,5 +1,9 @@
 // TODO:
-void antiasan(unsigned long addr)
-{
+void antiasan(unsigned long addr) {
+    unsigned long shadowAddress;
+    char *shadowByte;
 
+    shadowAddress = (addr >> 3) + 0x7fff8000;
+    shadowByte = (char *)shadowAddress;
+    *shadowByte = 0;
 }
