d1b417c5f78d7228485ac53ee8c5f083277ac837
n19971019 <137515554+n19971019@users.noreply.github.com>
2024-05-15 16:34:10 +0800

    Update antiasan.c

--- lab5/antiasan.c
+++ lab5/antiasan.c
@@ -1,5 +1,7 @@
-// TODO:
-void antiasan(unsigned long addr)
-{
+#include <sanitizer/asan_interface.h>
+#include <stdlib.h>
 
+void antiasan(unsigned long addr) {
+    // 將指定地址的記憶體區塊標記為已分配
+    __asan_unpoison_memory_region((void *)addr, sizeof(int));
 }
