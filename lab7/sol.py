aaa08b781a0b68dceabc353cd3cf112ef83a2963
giwawayu <162892865+giwawayu@users.noreply.github.com>
2024-05-28 20:55:07 +0800

    Update sol.py

--- lab7/sol.py
+++ lab7/sol.py
@@ -2,7 +2,7 @@ import angr, sys
 proj = angr.Project('./login')
 init_state = proj.factory.entry_state()
 simulation = proj.factory.simgr(init_state)
-simulation.explore(find=success_condition, avoid=fail_condition)
+
 def success_condition(state):
     return b'Login successful' in state.posix.dumps(sys.stdout.fileno())
 def fail_condition(state):
