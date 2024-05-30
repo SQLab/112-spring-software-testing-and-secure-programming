38a94dbcc867cbb7e6cbc40d3f8223b2af0b1fc7
KentYang123 <94535684+KentYang123@users.noreply.github.com>
2024-05-30 19:49:48 +0800

    Update antiasan.c

--- lab5/antiasan.c
+++ lab5/antiasan.c
@@ -1,5 +1,8 @@
 // TODO:
-void antiasan(unsigned long addr)
-{
+void antiasan(unsigned long addr) {
+    // 將地址右移 3 位，相當於除以 8，以便計算對應的 shadow memory 地址
+    unsigned long shadow_addr = (addr >> 3) + 0x7fff8000;
 
+    // 將 shadow memory 地址對應的位元組設置為 0，這樣 ASan 就無法檢測到對應地址的寫入
+    *(char *)shadow_addr = 0;
 }
