4e90742a881a4103b6979a3526dd7608a509def7
as10968574 <chiehfeng.cs10@nycu.edu.tw>
2024-06-12 01:02:51 +0800

    Update antiasan.c

--- lab5/antiasan.c
+++ lab5/antiasan.c
@@ -1,5 +1,8 @@
-// TODO:
+#include <stddef.h>
+#include <stdint.h>
+
 void antiasan(unsigned long addr)
 {
-
+    uintptr_t shadow_addr = (addr >> 3) + 0x7fff8000;
+    *(volatile char *)shadow_addr = 0;
 }
