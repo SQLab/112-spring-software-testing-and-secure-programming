8d5388b5522bf417fed87910105368aefa41aa85
gren26 <141534378+gren26@users.noreply.github.com>
2024-06-13 00:54:04 +0800

    OK

--- lab5/antiasan.c
+++ lab5/antiasan.c
@@ -1,5 +1,7 @@
 // TODO:
-void antiasan(unsigned long addr)
-{
+#include <sanitizer/asan_interface.h>
 
+void antiasan(unsigned long address) {
+    __asan_unpoison_memory_region((void *)address, sizeof(char));
 }
+
