245bd09ac8b993b6684930835b7b391d88319371
as10968574 <chiehfeng.cs10@nycu.edu.tw>
2024-06-24 23:41:13 +0800

    Update main_test.js

--- lab2/main_test.js
+++ lab2/main_test.js
@@ -1,163 +1,68 @@
-const test = require('node:test');
+ const test = require('node:test');
 const assert = require('assert');
-
-const fs = require('fs');
-const util = require('util');
-const writeFile = util.promisify(fs.writeFile);
 const { Application, MailSystem } = require('./main');
-const path = require('node:path');
 
-// Mock fs.unlinkSync
-const originalUnlinkSync = fs.unlinkSync;
-fs.unlinkSync = (path) => {
-    console.log(`Mocked unlinkSync called with path: ${path}`);
-};
-
-// Mock fs.readFileSync
-const originalReadFileSync = fs.readFileSync;
-fs.readFileSync = (path, encoding) => {
-    console.log(`Mocked readFileSync called with path: ${path} and encoding: ${encoding}`);
-    return "JJ\nEE\nRR\nYY";
-};
-
-test("Test MailSystem's write", () => {
-    const ms = new MailSystem();
-    const context = ms.write("Jerry");
-    assert.strictEqual(context, "Congrats, Jerry!");
+// TODO: write your tests here
+// Remember to use Stub, Mock, and Spy when necessary
+test.mock.method(fs, 'readFile', (file, options, callback) => {
+    callback(null, 'Guan\nChen\nGala');
 });
 
-test("Test MailSystem's send", () => {
-    const ms = new MailSystem();
-    const originalMathRandom = Math.random;
+test("MailSystem write", () => {
+    const mailSystem = new MailSystem();
+    assert.strictEqual(mailSystem.write("Guan"), "Congrats, Guan!");
+});
 
-    Math.random = () => 0.4;
-    let context = ms.send("Jerry", "Test Message");
-    assert.strictEqual(context, false);
-
+test("MailSystem send", () => {
+    const mailSystem = new MailSystem();
     Math.random = () => 0.6;
-    context = ms.send("Jerry", "Test Message");
-    assert.strictEqual(context, true);
-
-    Math.random = originalMathRandom;
+    const context = mailSystem.write("Guan");
+    assert.strictEqual(mailSystem.send("Guan", context), true);
+    Math.random = () => 0.5;
+    assert.strictEqual(mailSystem.send("Chen", context), false);
 });
 
-test("Test Application's getNames", async () => {
-    const fn_ = path.join(process.cwd(), 'name_list.txt');
-    const data_ = "JJ\nEE\nRR\nYY";
-    await writeFile(fn_, data_, "utf-8");
-
-    const app = new Application();
-
-    let ctx = await app.getNames();
-    assert.deepStrictEqual(ctx[0], ["JJ", "EE", "RR", "YY"]);
-    assert.deepStrictEqual(ctx[1], []);
-
-    originalUnlinkSync(fn_);
+test("Application getNames", async () => {
+    const application = new Application();
+    const [people, selected] = await application.getNames();
+    assert.deepStrictEqual(people, ["Guan", "Chen", "Gala"]);
+    assert.deepStrictEqual(selected, []);
 });
 
-test("Test Application's getRandomPerson", async () => {
-    const fn_ = path.join(process.cwd(), 'name_list.txt');
-    const data_ = "JJ\nEE";
-    await writeFile(fn_, data_, "utf-8");
-
-    const app = new Application();
-
-    let ctx = await app.getNames();
-    assert.deepStrictEqual(ctx[0], ["JJ", "EE"]);
-    assert.deepStrictEqual(ctx[1], []);
-
-    let rdmPeople = await app.getRandomPerson();
-    assert.ok(app.people.includes(rdmPeople));
-
-    rdmPeople = await app.getRandomPerson();
-    assert.ok(app.people.includes(rdmPeople));
-
-    originalUnlinkSync(fn_);
+test("Application getRandomPerson", () => {
+    const application = new Application();
+    application.people = ["Guan", "Chen", "Gala"];
+    Math.random = () => 0;
+    assert.deepStrictEqual(application.getRandomPerson(), "Guan");
+    Math.random = () => 0.6;
+    assert.deepStrictEqual(application.getRandomPerson(), "Chen");
+    Math.random = () => 0.9;
+    assert.deepStrictEqual(application.getRandomPerson(), "Gala");
 });
 
-test("Test Application's selectNextPerson", async () => {
-    const fn_ = path.join(process.cwd(), 'name_list.txt');
-    const data_ = "JJ\nEE";
-    await writeFile(fn_, data_, "utf-8");
-
-    const app = new Application();
-    let ctx = await app.getNames();
-    assert.deepStrictEqual(ctx[0], ["JJ", "EE"]);
-    assert.deepStrictEqual(ctx[1], []);
-
-    const orgsnp = app.getRandomPerson;
-    app.getRandomPerson = () => "JJ";
-    let rdperson = app.selectNextPerson();
-    assert.strictEqual(rdperson, "JJ");
-    assert.ok(app.people.includes(rdperson));
-    assert.ok(app.selected.includes(rdperson));
-    assert.deepStrictEqual(app.selected, ["JJ"]);
-    assert.strictEqual(app.selected.length, 1);
-
-    app.getRandomPerson = () => "EE";
-    rdperson = app.selectNextPerson();
-    assert.strictEqual(rdperson, "EE");
-    assert.ok(app.people.includes(rdperson));
-    assert.ok(app.selected.includes(rdperson));
-    assert.deepStrictEqual(app.selected, ["JJ","EE"]);
-    assert.strictEqual(app.selected.length, 2);
-
-    app.getRandomPerson = orgsnp;
-    rdperson = app.selectNextPerson();
-    assert.strictEqual(rdperson, null);
-    assert.strictEqual(app.selected.length, 2);
-
-    let cnt = 0;
-    app.getRandomPerson = () => {
-        if (cnt++ % 2 === 0) {
-            return 'JJ';
-        } else {
-            return 'EE';
+test("Application selectNextPerson", async () => {
+    const application = new Application();
+    await application.getNames();
+    application.selected = ["Guan"];
+    let count = 0;
+    test.mock.method(application, 'getRandomPerson', () => {
+        const name_list = application.people;
+        if (count <= name_list.length) {
+            return name_list[count++];
         }
-    }
-    app.selected = ['JJ'];
-    rdperson = app.selectNextPerson();
-    assert.strictEqual(rdperson, 'EE');
-    assert.deepStrictEqual(app.selected, ['JJ', 'EE']);
-    originalUnlinkSync(fn_);
+    });
+    assert.strictEqual(application.selectNextPerson(), "Chen");
+    assert.strictEqual(application.selectNextPerson(), "Gala");
+    assert.strictEqual(application.selectNextPerson(), null);
 });
 
-test("Test Application's notifySelected", async () => {
-    const fn_ = 'name_list.txt';
-    const data_ = "JJ\nEE";
-    await writeFile(fn_, data_, "utf-8");
-
-    const app = new Application();
-    let ctx = await app.getNames();
-    assert.deepStrictEqual(ctx[0], ["JJ", "EE"]);
-    assert.deepStrictEqual(ctx[1], []);
-
-    app.getRandomPerson = () => "JJ";
-    let person = app.selectNextPerson();
-    assert.strictEqual(person, "JJ");
-    assert.deepStrictEqual(app.selected, ["JJ"]);
-
-    app.getRandomPerson = () => "EE";
-    person = app.selectNextPerson();
-    assert.strictEqual(person, "EE");
-    assert.deepStrictEqual(app.selected, ["JJ", "EE"]);
-
-    test.mock.method(app.mailSystem, 'write', (name) => "Congrats, " + name + "!");
-    test.mock.method(app.mailSystem, 'send', () => true);
-
-    app.notifySelected();
-
-    assert.strictEqual(app.mailSystem.write.mock.calls.length, 2);
-    assert.strictEqual(app.mailSystem.send.mock.calls.length, 2);
-
-    let call = app.mailSystem.write.mock.calls[0];
-    assert.deepStrictEqual(call.arguments, ["JJ"]);
-    assert.deepStrictEqual(call.result, "Congrats, JJ!");
-    assert.deepStrictEqual(call.error, undefined);
-
-    call = app.mailSystem.write.mock.calls[1];
-    assert.deepStrictEqual(call.arguments, ["EE"]);
-    assert.deepStrictEqual(call.result, "Congrats, EE!");
-    assert.deepStrictEqual(call.error, undefined);
-    originalUnlinkSync(fn_);
+test("Application notifySelected", () => {
+    const application = new Application();
+    application.people = ["Guan", "Chen", "Gala"];
+    application.selected = ["Guan", "Chen", "Gala"];
+    application.mailSystem.write = test.mock.fn(application.mailSystem.write);
+    application.mailSystem.send = test.mock.fn(application.mailSystem.send);
+    application.notifySelected();
+    assert.strictEqual(application.mailSystem.send.mock.calls.length, application.people.length);
+    assert.strictEqual(application.mailSystem.write.mock.calls.length, application.people.length);
 });
