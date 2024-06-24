fc665fcad4590dfa05d3d913162319a38c205d9e
as10968574 <chiehfeng.cs10@nycu.edu.tw>
2024-06-24 22:31:37 +0800

    Update sol.py

--- lab7/sol.py
+++ lab7/sol.py
@@ -1,14 +1,14 @@
 import angr
+import sys
 proj = angr.Project('./login', auto_load_libs=False)
-
 start_state = proj.factory.entry_state()
 simgr = proj.factory.simgr(start_state)
 
 simgr.explore(find=lambda s: b'Login success' in s.posix.dumps(1))
 
 if simgr.found:
-    solution_state = simgr.found[0]
-    password = solution_state.posix.dumps(0).strip()
-    print(password.decode())  
+solution_state = simgr.found[0]
+password = solution_state.posix.dumps(0).strip()
+print(password.decode())
 else:
-    print("Password not found.")
+print("Password not found.")
